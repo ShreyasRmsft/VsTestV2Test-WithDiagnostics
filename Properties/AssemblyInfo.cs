d0ccfd1041346ae32ef816cf99d53d49e1eff4fc
DineshChirnanchu <Dinesh516ch@outlook.com>
2020-01-13 11:30:16 +0530

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StackOverflow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StackOverflow")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e12172aa-1f32-4d8c-bd6d-3cb2e0c6421d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StackOverflowTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StackOverflow
+{
+    [TestClass]
+    public class StackOverflowTests
+    {
+        [TestMethod]
+        public void RecusiveTest()
+        {
+            Recursive(780000000);
+        }
+
+        private void Recursive(int v)
+        {
+            Console.WriteLine(v);
+            Recursive(++v);
+        }
+    }
+}
